a383b6e889d71b1343774dcd12111c2e157deebd
Kazumasa Shimomura <kazu.devapp@gmail.com>
2020-05-13 23:09:56 +0900

    Refactor

--- ARPractice/Views/GreenBox.swift
+++ ARPractice/Views/GreenBox.swift
@@ -6,27 +6,28 @@ final class GreenBox: Entity, HasModel, HasCollision {
         didSet {
             guard let url = imageURL else { return }
 
-            components.set(makeModelComponent(for: url))
+            #warning("TODO: only change baseColor")
+            model?.materials[0] = makeMaterial(for: url)
         }
     }
 
     required init() {
         super.init()
 
-        components.set(makeModelComponent(for: nil))
+        components.set(ModelComponent(
+            mesh: .generateBox(size: [0.05, 0.01, 0.15]),
+            materials: [makeMaterial(for: nil)]
+        ))
         generateCollisionShapes(recursive: true)
     }
 
-    func makeModelComponent(for imageURL: URL?) -> ModelComponent {
+    private func makeMaterial(for imageURL: URL?) -> SimpleMaterial {
         var material = SimpleMaterial(color: .green, isMetallic: false)
         if let resource = imageURL.map ({ try? TextureResource.load(contentsOf: $0) })
             ?? (try? TextureResource.load(named: "risu")) {
             material.baseColor = MaterialColorParameter.texture(resource)
         }
 
-        return ModelComponent(
-            mesh: .generateBox(size: [0.05, 0.01, 0.15]),
-            materials: [material]
-        )
+        return material
     }
 }
