b2f020014ae15a6ede040b27e34187c5dc9d98b4
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-06-11 10:20:11 +0900

    Install RealityUI

--- ARPractice/AppDelegate.swift
+++ ARPractice/AppDelegate.swift
@@ -7,7 +7,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = TestUIViewController()
 
         return true
     }
